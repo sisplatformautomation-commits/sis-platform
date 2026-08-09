19f7840a4aae3070e488538660eabbb98f327b51
sisplatformautomation-commits <sis.platform.automation@gmail.com>
2026-08-09 20:08:15 +0200

    feat: export Brandenburg connector API

---
+++ src/brandenburg/index.js
@@ -0,0 +1,3 @@
+export { normalizeXPlanProperties, summarizeBPlanFeature, hasStructuredBuildingMetrics } from "./xplan.js";
+export { buildWfsGetFeatureUrl, fetchBPlanAtPoint, fetchFirstBPlanWithMetrics } from "./wfs.js";
+export { addLocationMarker, addBPlanGeoJson, waitForMpJsApi } from "./viewer.js";
